15fcec5c5c5dbf25038016c2eba196a13a88d001
Patryk Zadroga <Patryk.Zadroga@amazemet.com>
2024-08-01 12:31:00 +0200

    fix references

--- src/ArduinoAdsLib.cpp
+++ src/ArduinoAdsLib.cpp
@@ -1,10 +1,9 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-// #include <string.h>
 #include <stdio.h>
 
-#include "EspAdsLib.h"
+#include "ArduinoAdsLib.h"
 
 // Constructor
 ADS::AmsAddr::AmsAddr(char* NetId, uint16_t u16Port)
